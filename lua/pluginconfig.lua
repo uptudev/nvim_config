2c36b0d3f1ee160965bb82783a0c734360cdc41b
uptu <91502176+uptudev@users.noreply.github.com>
2024-03-11 19:19:03 -0600

    Update pluginconfig.lua
    Duplicate of nvim-tree/nvim-web-devicons

--- lua/pluginconfig.lua
+++ lua/pluginconfig.lua
@@ -9,7 +9,6 @@ require('packer').startup(function(use)
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
   use 'Yggdroot/indentLine'
   use 'tpope/vim-fugitive'
   use 'junegunn/gv.vim'
